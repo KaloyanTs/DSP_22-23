12b97c7ff8227f3797cb10d364426c524fb33ea3
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-10-30 12:14:07 +0200

    working on tom and jerry

--- 28oct/main.cpp
+++ 28oct/main.cpp
@@ -3,8 +3,8 @@
 #include <stack>
 #include <utility>
 
-const unsigned HOME_WIDTH = 10;
-const unsigned HOME_HEIGHT = 10;
+const unsigned HOME_WIDTH = 2;
+const unsigned HOME_HEIGHT = 2;
 
 template <typename T>
 struct qStack
@@ -111,26 +111,55 @@ unsigned ArthurIsCoward(unsigned n)
     return q.front();
 }
 
-std::queue<std::pair<unsigned, unsigned>> saveJerry(unsigned n, unsigned m)
+using pos = std::pair<unsigned, unsigned>;
+std::stack<pos> saveJerry(bool home[][HOME_WIDTH])
 {
+    struct visCell
+    {
+        pos prev;
+        pos current;
+        visCell(const pos &c = {0, 0}, const pos &p = {-1, -1}) : prev(p), current(c) {}
+    };
+    std::queue<visCell> q;
+    std::stack<visCell> past;
 
-    std::queue<std::queue<std::pair<unsigned, unsigned>>> paths;
-    std::queue<std::pair<unsigned, unsigned>> curPath;
-    // curPath.push({0, 0});
-    // paths.push(curPath);
-    // while (!paths.empty())
-    // {
-    //     curPath = paths.front();
-    //     if (curPath.front() == std::pair<unsigned, unsigned>({n - 1, m - 1}))
-    //         return curPath;
-    //     paths.pop();
-    //     for (unsigned dx = -1; dx <= 1; dx += 2)
-    //         for (unsigned dy = -1; dy <= 1; dy += 2)
-    //         {
-    //             curPath.push({})
-    //         }
-    // }
-    return paths.front();
+    q.push(visCell());
+    visCell c;
+    while (!q.empty())
+    {
+        c = q.front();
+        q.pop();
+        if (c.current == pos({HOME_WIDTH - 1, HOME_HEIGHT - 1}))
+        {
+            std::stack<pos> res;
+            res.push(c.current);
+            res.push(c.prev);
+            while (!past.empty())
+            {
+                while (c.prev.first != -1 && c.prev.second != -1 && past.top().current != c.prev)
+                {
+                    std::cout << '(' << past.top().current.first << ',' << past.top().current.second << ')' << ' ';
+                    past.pop();
+                }
+                c.prev = past.top().prev;
+                past.pop();
+                res.push(c.prev);
+            }
+            return res;
+        }
+        if (!(c.current.first < 0 || c.current.first >= HOME_WIDTH ||
+              c.current.second < 0 || c.current.second >= HOME_HEIGHT ||
+              home[c.current.first][c.current.second]))
+        {
+            home[c.current.first][c.current.second] = true;
+            past.push(c);
+            c.prev = c.current;
+            for (int dx = -1; dx <= 1; dx += 2)
+                for (int dy = -1; dy <= 1; dy += 2)
+                    q.push(pos({c.current.first + dx, c.current.second + dy}));
+        }
+    }
+    return std::stack<pos>();
 }
 
 int main()
@@ -167,5 +196,36 @@ int main()
         stQ.pop();
     }
     std::cout << '\n';
+
+    // bool home[HOME_HEIGHT][HOME_WIDTH] =
+    //     {
+    //         {0, 1, 0, 0, 0, 0, 1, 1, 0, 1},
+    //         {0, 0, 0, 1, 1, 0, 0, 1, 0, 1},
+    //         {0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+    //         {1, 1, 0, 0, 0, 1, 0, 1, 0, 1},
+    //         {0, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+    //         {1, 0, 0, 0, 0, 0, 1, 1, 0, 0},
+    //         {0, 0, 0, 0, 0, 0, 1, 1, 0, 1},
+    //         {0, 1, 0, 0, 0, 0, 1, 0, 0, 0},
+    //         {0, 0, 0, 0, 0, 1, 0, 1, 0, 0},
+    //         {1, 1, 0, 0, 0, 1, 0, 0, 0, 0},
+    //     };
+
+    bool home[HOME_HEIGHT][HOME_WIDTH] =
+        {
+            {0, 1},
+            {0, 0},
+        };
+
+    std::stack<pos> path = saveJerry(home);
+    while (!path.empty())
+    {
+        std::cout << '(' << path.top().first << ',' << path.top().second << ')';
+        path.pop();
+        if (!path.empty())
+            std::cout << " -> ";
+    }
+    std::cout << '\n';
+
     return 0;
 }
