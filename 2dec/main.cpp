7e0a95f44fc3361cce8f357998fa56e95cef5cba
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-12-02 20:44:57 +0200

    finished exercise problems

--- 2dec/main.cpp
+++ 2dec/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <list>
 
 template <typename T>
@@ -88,11 +89,51 @@ bool pathToLeafWithSum(const Node<T> *root, int sum)
     return false;
 }
 
+using Folder = Node<std::string>;
+
+std::string pathTo(const Folder *root, const std::string &to)
+{
+    if (!root)
+        return "";
+    if (root->children.empty() && root->data == to)
+        return to;
+    if (root->children.empty())
+        return "";
+    std::string tmp;
+    for (const Folder *f : root->children)
+    {
+        tmp = pathTo(f, to);
+        if (tmp != "")
+            return root->data + "/" + tmp;
+    }
+    return "";
+}
+
+std::string findRelativePath(const Folder *root,
+                             const std::string &from,
+                             const std::string &to)
+{
+    if (!root)
+        return "";
+    std::string res;
+    for (const Folder *f : root->children)
+        if (f->data == from)
+        {
+            for (const Folder *ff : f->children)
+            {
+                res = pathTo(ff, to);
+                if (res != "")
+                    return "./" + res;
+            }
+        }
+    return "";
+}
+
 using intTree = Node<int>;
 
 int main()
 {
-    intTree *root = new intTree{1,
+    intTree *root = new intTree{9,
                                 {new intTree{2},
                                  new intTree{3},
                                  new intTree{4}}};
@@ -103,7 +144,25 @@ int main()
               << std::boolalpha << hasRootEqSumOfChildren(root) << '\n';
     printHeightN(2, root);
     std::cout << '\n';
+    std::cout << "path to leaf with sum 4: "
+              << std::boolalpha
+              << pathToLeafWithSum(root, 4) << '\n';
+    std::cout << "has root equal to sum of children: "
+              << std::boolalpha << hasRootEqSumOfChildren(root) << '\n';
+    std::cout << '\n';
 
     clear(root);
+
+    Folder *f = new Folder{"",
+                           {new Folder{"bin", {
+                                                  new Folder{"cp"},
+                                                  new Folder{"ksh"},
+                                                  new Folder{"is"},
+                                                  new Folder{"pwd"},
+                                              }},
+                            new Folder{"dev"}, new Folder{"etc", {new Folder{"passwd"}}}, new Folder{"home", {new Folder{"mthomas", {new Folder{"bin"}, new Folder{"class_stuff", {new Folder{"foo"}, new Folder{"bar"}}}, new Folder{"profile"}}}}}, new Folder{"lib"}, new Folder{"mnt"}, new Folder{"proc"}, new Folder{"root"}, new Folder{"sbin"}, new Folder{"tmp"}, new Folder{"usr", {new Folder{"bin"}}}}};
+
+    std::cout << findRelativePath(f, "home", "bar") << '\n';
+    clear(f);
     return 0;
 }
