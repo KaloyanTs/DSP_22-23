5d4448935833fa3808ff9b5d6ba95ff5eea7666c
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-12-24 01:45:35 +0200

    debugging with main

--- Project/main.cpp
+++ Project/main.cpp
@@ -3,3 +3,13 @@
 
 #include "SkipList_tests.hpp"
 #include "Graph_tests.hpp"
+
+// #include "SkipList.hpp"
+// #include "TrainMap.hpp"
+// #include "Graph.hpp"
+// #include <sstream>
+
+// int main()
+// {
+//     return 0;
+// }
