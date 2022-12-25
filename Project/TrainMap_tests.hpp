57facb1f87012ef464d52661f3ef34dacbce0272
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-12-25 20:08:46 +0200

    testing 1a

--- Project/TrainMap_tests.hpp
+++ Project/TrainMap_tests.hpp
@@ -7,38 +7,22 @@
 #include <iostream>
 #include <list>
 
-
 TEST_CASE("Journey through sequence of cities.")
 {
-    TrainMap tm;
-    std::string cities[] = {"Sofia", "Pazardzhik", "Plovdiv", "Dimitrovgrad", "StaraZagora", "NovaZagora", "Yambol", "Karnobat", "Burgas"};
-    for (const std::string &s : cities)
-        tm.push_back(s);
-    tm.addLink(cities[0], cities[2]);
-    tm.addLink(cities[2], cities[5]);
-    tm.addLink(cities[3], cities[5]);
-    tm.addLink(cities[4], cities[6]);
-    tm.addLink(cities[5], cities[8]);
-
     SUBCASE("Journey through Plovdiv, StaraZagora and Yambol.")
     {
-        std::list<std::string> citiesToVisit;
-        citiesToVisit.push_back(cities[2]);
-        citiesToVisit.push_back(cities[4]);
-        citiesToVisit.push_back(cities[6]);
-        std::list<std::string> p = tm.shortestJourney(citiesToVisit);
+        std::list<std::string> res = TrainMap::solveShortestJourney("SofiaBurgas.txt", "test1.txt");
         std::ostringstream s;
-        for (const std::string &city : p)
+        for (const std::string &city : res)
             s << city << '\n';
         CHECK_EQ(s.str(), "Sofia\nPlovdiv\nDimitrovgrad\nStaraZagora\nYambol\nKarnobat\nBurgas\n");
     }
 
     SUBCASE("Journey through no cities.")
     {
-        std::list<std::string> citiesToVisit;
-        std::list<std::string> p = tm.shortestJourney(citiesToVisit);
+        std::list<std::string> res = TrainMap::solveShortestJourney("SofiaBurgas.txt", "test2.txt");
         std::ostringstream s;
-        for (const std::string &city : p)
+        for (const std::string &city : res)
             s << city << '\n';
         CHECK_EQ(s.str(), "Sofia\nPlovdiv\nNovaZagora\nBurgas\n");
     }
