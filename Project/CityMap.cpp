883459536b71bc264ce56170691d09c6bc3e083f
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-12-25 19:32:17 +0200

    implemening reading city from a file

--- Project/CityMap.cpp
+++ Project/CityMap.cpp
@@ -1,9 +1,28 @@
 #include "CityMap.hpp"
+#include <fstream>
+#include <iostream>
 
 std::string CityMap::start = "Railstation";
+const unsigned CITY_MAX_LENGTH = 128;
 
-CityMap CityMap::readFromFile(const std::string &)
+CityMap CityMap::readFromFile(const std::string &path)
 {
-    // todo check whether move semantics should be written for Graph
-    return CityMap();
+    std::ifstream ifs;
+    ifs.open(std::string("data\\") + path, std::ios::in);
+    CityMap res;
+    if (!ifs.is_open())
+        return res;
+    unsigned cities, edges, cost;
+    ifs >> cities >> edges;
+    char buf1[CITY_MAX_LENGTH];
+    char buf2[CITY_MAX_LENGTH];
+    for (unsigned i = 0; i < edges; ++i)
+    {
+        ifs.ignore();
+        ifs >> buf1 >> buf2 >> cost;
+        res.addEdge(buf1, buf2, cost);
+    }
+    ifs >> res.timeLimit;
+    ifs.close();
+    return res;
 }
