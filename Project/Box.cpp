35c33a4218131d040c6f62e0400e5082bffa2108
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-12-26 14:12:31 +0200

    separating Box header and implementation

---
+++ Project/Box.cpp
@@ -0,0 +1,109 @@
+#include "Box.hpp"
+
+void Box::print(std::ostream &os, unsigned level) const
+{
+    std::string indent = "";
+    for (unsigned i = 0; i < level; ++i)
+        indent += '\t';
+    os << indent << name << ":\n";
+    for (const std::string &s : souvenirs)
+        os << indent << '\t' << s << '\n';
+    for (const Box *b : boxes)
+        b->print(os, level + 1);
+}
+
+Box::Box(const Box &other) : name(other.name), souvenirs(other.souvenirs), isContained(other.isContained)
+{
+    for (Box *b : other.boxes)
+        boxes.push_back(new Box(*b));
+}
+
+Box::~Box()
+{
+    for (Box *b : boxes)
+        delete b;
+}
+
+std::list<Box> Box::readFromFile(const std::string &path)
+{
+    std::ifstream ifs;
+    ifs.open(std::string("data\\") + path);
+    if (!ifs.is_open())
+        return std::list<Box>();
+    unsigned boxCount, count;
+    ifs >> boxCount;
+    ifs.ignore();
+    std::string buf;
+
+    using signedBox = std::pair<std::string, Box *>;
+    std::map<std::string, Box *> data;
+
+    //                  where   what
+    std::queue<std::pair<Box *, std::string>> q;
+    for (unsigned i = 0; i < boxCount; ++i)
+    {
+        ifs >> buf;
+        Box *current = new Box(buf);
+        data.insert(signedBox(buf, current));
+        ifs >> count;
+        ifs.ignore();
+        for (unsigned j = 0; j < count; ++j)
+        {
+            ifs >> buf;
+            current->addSouvenir(buf);
+        }
+        ifs >> count;
+        ifs.ignore();
+        for (unsigned j = 0; j < count; ++j)
+        {
+            ifs >> buf;
+            q.push(std::pair<Box *, std::string>(current, buf));
+        }
+    }
+    ifs.close();
+
+    while (!q.empty())
+    {
+        std::pair<Box *, std::string> &task = q.front();
+        Box *toBePut = data[task.second];
+        task.first->addBox(toBePut);
+        toBePut->isContained = true;
+        q.pop();
+    }
+
+    std::map<std::string, Box *>::iterator cleaner = data.begin();
+    while (cleaner != data.end())
+    {
+        if (cleaner->second->isContained)
+            data.erase(cleaner++);
+        else
+            ++cleaner;
+    }
+    std::list<Box> res;
+    for (std::pair<std::string, Box *> p : data)
+    {
+        res.push_back(*p.second);
+        delete p.second;
+    }
+    return res;
+}
+Box &Box::addSouvenir(const std::string &s)
+{
+    souvenirs.push_back(s);
+    return *this;
+}
+Box &Box::addBox(Box *b)
+{
+    boxes.push_back(b);
+    return *this;
+}
+
+void Box::optimize(Box &b)
+{
+}
+
+std::ostream &operator<<(std::ostream &os, const Box &b)
+{
+    b.print(os, 0);
+    return os;
+}
--- Project/Box.hpp
+++ Project/Box.hpp
@@ -14,115 +14,19 @@ class Box
     std::list<Box *> boxes;
     bool isContained;
 
-    void print(std::ostream &os, unsigned level) const
-    {
-        std::string indent = "";
-        for (unsigned i = 0; i < level; ++i)
-            indent += '\t';
-        os << indent << name << ":\n";
-        for (const std::string &s : souvenirs)
-            os << indent << '\t' << s << '\n';
-        for (const Box *b : boxes)
-            b->print(os, level + 1);
-    }
+    void print(std::ostream &os, unsigned level) const;
 
 public:
     Box(const std::string &_name = "Unlabeled") : name(_name), isContained(false) {}
-    Box(const Box &other) : name(other.name), souvenirs(other.souvenirs), isContained(other.isContained)
-    {
-        for (Box *b : other.boxes)
-            boxes.push_back(new Box(*b));
-    }
+    Box(const Box &other);
     Box &operator=(const Box &) = delete;
-    ~Box()
-    {
-        for (Box *b : boxes)
-            delete b;
-    }
+    ~Box();
 
-    static std::list<Box> readFromFile(const std::string &path)
-    {
-        std::ifstream ifs;
-        ifs.open(std::string("data\\") + path);
-        if (!ifs.is_open())
-            return std::list<Box>();
-        unsigned boxCount, count;
-        ifs >> boxCount;
-        ifs.ignore();
-        std::string buf;
-
-        using signedBox = std::pair<std::string, Box *>;
-        std::map<std::string, Box *> data;
-
-        //                  where   what
-        std::queue<std::pair<Box *, std::string>> q;
-        for (unsigned i = 0; i < boxCount; ++i)
-        {
-            ifs >> buf;
-            Box *current = new Box(buf);
-            data.insert(signedBox(buf, current));
-            ifs >> count;
-            ifs.ignore();
-            for (unsigned j = 0; j < count; ++j)
-            {
-                ifs >> buf;
-                current->addSouvenir(buf);
-            }
-            ifs >> count;
-            ifs.ignore();
-            for (unsigned j = 0; j < count; ++j)
-            {
-                ifs >> buf;
-                q.push(std::pair<Box *, std::string>(current, buf));
-            }
-        }
-        ifs.close();
-
-        while (!q.empty())
-        {
-            std::pair<Box *, std::string> &task = q.front();
-            Box *toBePut = data[task.second];
-            task.first->addBox(toBePut);
-            toBePut->isContained = true;
-            q.pop();
-        }
-
-        std::map<std::string, Box *>::iterator cleaner = data.begin();
-        while (cleaner != data.end())
-        {
-            std::cout << cleaner->first << '\n';
-            if (cleaner->second->isContained)
-                data.erase(cleaner++);
-            else
-                ++cleaner;
-        }
-        std::list<Box> res;
-        for (std::pair<std::string, Box *> p : data)
-        {
-            res.push_back(*p.second);
-            delete p.second;
-        }
-        return res;
-    }
-    // todo implement
-    static void optimize(Box &b);
-    Box &addSouvenir(const std::string &s)
-    {
-        souvenirs.push_back(s);
-        return *this;
-    }
-    Box &addBox(Box *b)
-    {
-        boxes.push_back(b);
-        return *this;
-    }
+    static std::list<Box> readFromFile(const std::string &path);
+    void optimize(Box &b);
+    Box &addSouvenir(const std::string &s);
+    Box &addBox(Box *b);
     friend std::ostream &operator<<(std::ostream &os, const Box &b);
 };
-
-std::ostream &operator<<(std::ostream &os, const Box &b)
-{
-    b.print(os, 0);
-    return os;
-}
 
 #endif
