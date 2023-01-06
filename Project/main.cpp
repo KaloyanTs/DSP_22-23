9a45b5dc932b8a5534969669790bfe9f36c2dea5
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2023-01-06 18:47:59 +0200

    repair main

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,18 +1,18 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+// #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
-#include "tests/SkipList_tests.hpp"
-#include "tests/Graph_tests.hpp"
-#include "tests/TrainMap_tests.hpp"
-#include "tests/CityMap_tests.hpp"
-#include "tests/Box_tests.hpp"
+// #include "tests/SkipList_tests.hpp"
+// #include "tests/Graph_tests.hpp"
+// #include "tests/TrainMap_tests.hpp"
+// #include "tests/CityMap_tests.hpp"
+// #include "tests/Box_tests.hpp"
 
-// #include "TaskSolver.hpp"
+#include "TaskSolver.hpp"
 
-// int main()
-// {
-//     TaskSolver::total();
-//     std::cout << "Enter any key to exit...";
-//     std::cin.ignore();
-//     std::cin.get();
-//     return 0;
-// }
+int main()
+{
+    TaskSolver::total();
+    std::cout << "Enter any key to exit...";
+    std::cin.ignore();
+    std::cin.get();
+    return 0;
+}
