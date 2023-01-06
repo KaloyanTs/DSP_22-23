4599a88127d6be9a2f708dd445440b602bb0cfcc
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2023-01-06 15:27:53 +0200

    graph using map

--- 6jan/mapGraph.cpp
+++ 6jan/mapGraph.cpp
@@ -4,15 +4,14 @@
 #include <set>
 #include <vector>
 
-// using GraphAdjList = std::map<std::string, std::forward_list<std::string>>;
-using GraphAdjList = std::vector<std::vector<int>>;
+using GraphAdjList = std::map<const std::string, std::forward_list<std::string>>;
 
-void visit(const GraphAdjList &g, int from, std::set<int> &visited)
+void visit(GraphAdjList &g, const std::string &from, std::set<std::string> &visited)
 {
     if (visited.find(from) != visited.end())
         return;
     visited.insert(from);
-    for (int to : g[from])
+    for (const std::string &to : g[from])
     {
         if (visited.find(to) == visited.end())
         {
@@ -21,27 +20,27 @@ void visit(const GraphAdjList &g, int from, std::set<int> &visited)
     }
 }
 
-int countComponents(const GraphAdjList &g)
+int countComponents(GraphAdjList &g)
 {
     size_t count = 0;
 
-    std::set<int> visited;
+    std::set<std::string> visited;
 
-    for (unsigned i = 0; i < g.size(); ++i)
-        if (visited.find(i) == visited.end())
+    for (const std::pair< std::string, std::forward_list< std::string>> &i : g)
+        if (visited.find(i.first) == visited.end())
         {
-            visit(g, i, visited);
+            visit(g, i.first, visited);
             ++count;
         }
     return count;
 }
 
-bool traverse(const GraphAdjList &g, int prev, int from, std::set<int> &visited)
+bool traverse(GraphAdjList &g, const std::string &prev, const std::string &from, std::set<std::string> &visited)
 {
-    if (visited.find(from) != visited.end() && prev != -1)
+    if (visited.find(from) != visited.end() && prev != "")
         return true;
     visited.insert(from);
-    for (int to : g[from])
+    for (const std::string &to : g[from])
     {
         if (to != prev && traverse(g, from, to, visited))
             return true;
@@ -49,36 +48,36 @@ bool traverse(const GraphAdjList &g, int prev, int from, std::set<int> &visited)
     return false;
 }
 
-bool isCyclic(const GraphAdjList &g)
+bool isCyclic(GraphAdjList &g)
 {
-    std::set<int> visited;
-    unsigned i = 0;
-    while (i < g.size() && !(visited.find(i) == visited.end() && traverse(g, -1, i, visited)))
-        ++i;
-    return i < g.size();
+    std::set<std::string> visited;
+
+    for (const std::pair< std::string, std::forward_list< std::string>> &from : g)
+    {
+        if (visited.find(from.first) == visited.end() && traverse(g, "", from.first, visited))
+            return true;
+    }
+    return false;
 }
 
-void addEdge(GraphAdjList &g, int from, int to)
+void addEdge(GraphAdjList &g, const std::string &from, const std::string &to)
 {
-    g[from].push_back(to);
-    g[to].push_back(from);
+    g[from].push_front(to);
+    g[to].push_front(from);
 }
 
 int main()
 {
-    GraphAdjList g(8);
-    addEdge(g, 0, 1);
-    addEdge(g, 0, 2);
-    addEdge(g, 3, 4);
-    addEdge(g, 3, 6);
-    addEdge(g, 4, 6);
-    addEdge(g, 5, 7);
+    GraphAdjList g;
+    addEdge(g, "A", "B");
+    addEdge(g, "A", "C");
+    addEdge(g, "B", "D");
+    addEdge(g, "C", "D");
+    addEdge(g, "D", "E");
+    addEdge(g, "D", "F");
 
     std::cout << countComponents(g) << '\n';
     std::cout << isCyclic(g) << '\n';
 
     return 0;
 }
-
-//todo use  map
-//          unordered_map
