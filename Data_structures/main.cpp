736d606bd382b04cc76aa25ff591c8c78cd720ad
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2023-01-29 15:43:59 +0200

    all tests

--- Data_structures/main.cpp
+++ Data_structures/main.cpp
@@ -3,9 +3,9 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-// #include "stack_tests.hpp"
-// #include "queue_tests.hpp"
-// #include "lists_tests.hpp"
-// #include "trees_tests.hpp"
-#include "graph_tests.hpp" //todo
-                           // #include "dictionary_tests.hpp"
+#include "stack_tests.hpp"
+#include "queue_tests.hpp"
+#include "lists_tests.hpp"
+#include "trees_tests.hpp"
+#include "graph_tests.hpp"
+#include "dictionary_tests.hpp"
